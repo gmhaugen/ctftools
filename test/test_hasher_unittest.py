98919b397c6ab570bc8eddbea79d5d502a2587c3
gmhaugen <geirmash@gmail.com>
2022-12-15 14:14:19 +0100

    fixed test case for sha384, asserted with wrong hash

--- test/test_hasher_unittest.py
+++ test/test_hasher_unittest.py
@@ -17,7 +17,7 @@ class TestHasher(unittest.TestCase):
         self.assertEqual(hasher.hashsha256("hello world"), "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9")
     
     def test_hashsha384(self):
-        self.assertEqual(hasher.hashsha384("hello world"), "83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b")
+        self.assertEqual(hasher.hashsha384("hello world"), "fdbd8e75a67f29f701a4e040385e2e23986303ea10239211af907fcbb83578b3e417cb71ce646efd0819dd8c088de1bd")
     
     def test_hashsha512(self):
         self.assertEqual(hasher.hashsha512("hello world"), "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f")
